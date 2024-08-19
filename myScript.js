e886f5a2af08c84eba3e5fb292f79a6a21c91b16
khalil <khalilcode01@gmail.com>
2024-08-19 16:04:59 +0300

    Code the function permute which take the plain text and transform the bits based on the initial permutation table

--- myScript.js
+++ myScript.js
@@ -1,10 +1,27 @@
 function hexToBinary(hexaString){
+    
     let binaryString = '';
 
     for (let i = 0; i < hexaString.length; i++){
         let binaryText = parseInt(hexaString[i], 16).toString(2).padStart(4, '0');
         binaryString += binaryText;
     }
-    
+
     return binaryString;
+}
+function permute(plainText, permutationTable) {
+    return permutationTable.map(index => plainText[index - 1]).join('');
+}
+
+function encrypt(plainText, key) {
+    
+    const initialPermutation = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 
+        28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 
+        64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 
+        25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 
+        61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 
+        31, 23, 15, 7];
+
+    let permuted = permute(plainText, initialPermutation);
+    
 }
