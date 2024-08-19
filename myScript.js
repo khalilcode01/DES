8c678d7ea283ee131a92d57e4125d559b9f06c41
khalil <khalilcode01@gmail.com>
2024-08-19 16:43:48 +0300

    Code the function parityDrop to drop the parity bits in the key

--- myScript.js
+++ myScript.js
@@ -10,7 +10,29 @@ function hexToBinary(hexaString){
     return binaryString;
 }
 function permute(plainText, permutationTable) {
+    
     return permutationTable.map(index => plainText[index - 1]).join('');
+
+}
+
+function f(right, key) {
+    
+    const expansionTable = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 
+        12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 
+        21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 
+        28, 29, 30, 31, 32, 1];
+    
+}
+
+function parityDrop(key){
+    let i = 0;
+    let dropedKey = '';
+    for(let i = 0; i < 64; i++){
+        if(i+1 % 8 == 0)
+            continue
+        dropedKey += key[i];
+    }
+    return dropedKey;
 }
 
 function encrypt(plainText, key) {
@@ -23,5 +45,19 @@ function encrypt(plainText, key) {
         31, 23, 15, 7];
 
     let permuted = permute(plainText, initialPermutation);
+    
+    let leftPlainText;
+    
+    let rightPlainText;
+    
+    for(let i = 0; i < 64; i++){
+        
+        if(i < 32)
+            leftPlainText += permuted[i];
+        
+        else if (i > 32 && i < 64)
+            rightPlainText += permuted[i];
+    
+    }
     
 }
