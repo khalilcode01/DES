ef9034fabd2e69f4811526badbbff47fd4e92e6f
khalil <khalilcode01@gmail.com>
2024-08-20 07:51:37 +0300

    Update

--- myScript.js
+++ myScript.js
@@ -19,43 +19,41 @@ function permute(plainText, permutationTable) {
 }
 
 function circularShiftLeft(str, shiftAmount) {
-    
-    let n = str.length;
-    
-    let shift = shiftAmount % n;
-
-    let shiftedString = str.slice(shift) + str.slice(0, shift);
-    
-    return shiftedString;
+    let shift = shiftAmount % str.length;
+    return str.slice(shift) + str.slice(0, shift);
 }
 
 function permuteKey(key, permutationTable, roundNumber) {
     
+    const finalPermutationTable = [
+        40, 8, 48, 16, 56, 24, 64, 32,
+        39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30,
+        37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28,
+        35, 3, 43, 11, 51, 19, 59, 27,
+        34, 2, 42, 10, 50, 18, 58, 26,
+        33, 1, 41, 9, 49, 17, 57, 25
+    ];
+
     const shiftTable = [
         1, 1, 2, 2, 2, 2, 2, 2,
         1, 2, 2, 2, 2, 2, 2, 1
     ];
-
-    let wholeKey = '';
     
-    leftKey = key.slice(0, 28);
-    
-    rightKey = key.slice(28, 56);
-    
-    leftKey = circularShiftLeft(leftKey, shiftTable[roundNumber - 1]);
-    
-    rightKey = circularShiftLeft(rightKey, shiftTable[roundNumber - 1]);
-    
-    combinedKey = leftKey + rightKey;
+    combinedKey = shiftLeft(key, roundNumber);
 
     return permute(combinedKey, permutationTable);
+    
 }
+
+
 
 function xOr(expandedRight, roundKey) {
     return expandedRight.split('').map((bit, index) => bit ^ roundKey[index]).join('');
 }
 
-function f(right, key, round) {
+function f(right, key) {
     
     const expansionTable = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 
         12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 
@@ -128,19 +126,18 @@ function f(right, key, round) {
         18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 
         30, 6, 22, 11, 4, 25];
 
-    roundKey = generateRoundKey(key, pc2Table, round);
-
     let expandedRight = permute(right, expansionTable);
     
-    let xor = xOr(expandedRight, roundKey);
+    let xorWithKey = xOr(expandedRight, key);
     
     let result = '';
 
     for (let i = 0; i < 8; i++) {
-        let block = xor.slice(i * 6, (i + 1) * 6);
+        let block = xorWithKey.slice(i * 6, (i + 1) * 6);
         let row = parseInt(block[0] + block[5], 2);
         let col = parseInt(block.slice(1, 5), 2);
-        result += ('0000' + sBoxes[i][row * 16 + col].toString(2)).slice(-4);
+        let output = sBoxes[i][row * 16 + col].toString(2).padStart(4, '0');
+        result += output.toString(2).padStart(4, '0');
     }
     
     return permute(result, permutationTable);
@@ -151,52 +148,54 @@ function f(right, key, round) {
 
 function parityDrop(key){
 
-    let dropedKey = '';
+    const pc1 = [
+        57, 49, 41, 33, 25, 17, 9,
+        1,  58, 50, 42, 34, 26, 18,
+        10, 2,  59, 51, 43, 35, 27,
+        19, 11, 3,  60, 52, 44, 36,
+        63, 55, 47, 39, 31, 23, 15,
+        7,  62, 54, 46, 38, 30, 22,
+        14, 6,  61, 53, 45, 37, 29,
+        21, 13, 5,  28, 20, 12, 4
+    ];
 
-    for(let i = 0; i < 64; i++){
-        
-        if(i + 1 % 8 == 0)
-            continue
-        
-        dropedKey += key[i];
-    
-    }
-
-    return dropedKey;
+    return permute(key, pc1);
 
 }
 
-function generateRoundKey(key) {
+function generateRoundKeys(key) {
+    const subkeys = [];
+    let leftKey = key.slice(0, 28);
+    let rightKey = key.slice(28, 56);
+
+    const shiftTable = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1];
     
     const pc2Table = [
-        14, 17, 11, 24, 1, 5, 3, 28,
-        15, 6, 21, 10, 23, 19, 12, 4,
-        26, 8, 16, 7, 27, 20, 13, 2,
-        41, 52, 31, 37, 47, 55, 30, 40,
-        51, 45, 33, 48, 44, 49, 39, 56,
-        34, 53, 46, 42, 50, 36, 29, 32
+        14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
+        23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
+        41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
+        44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32
     ];
 
-    let subkeys = [];
-
-    for (let round = 1; round <= 16; round++) {
-        subkeys.push(permuteKey(key, pc2Table, round));
+    for (let round = 0; round < 16; round++) {
+        leftKey = circularShiftLeft(leftKey, shiftTable[round]);
+        rightKey = circularShiftLeft(rightKey, shiftTable[round]);
+        const combinedKey = leftKey + rightKey;
+        subkeys.push(permute(combinedKey, pc2Table));
     }
-
     return subkeys;
 }
 
 function roundEncrypt(leftPlainText, rightPlainText, roundKey) {
-    round++;
-    roundLeft = f(rightPlainText, roundKey);
-    xored = xOr(roundLeft, rightPlainText);
+    let roundright = f(rightPlainText, roundKey);
+    let xored = xOr(leftPlainText, roundright);
     leftPlainText = rightPlainText;
     rightPlainText = xored;
-    
+    return [leftPlainText, rightPlainText];
 }
 
-function encrypt(plainText, key) {
-    let round = 0;
+function encryptBlock(plainText, keys) {
+    
     const initialPermutation = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 
         28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 
         64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 
@@ -214,36 +213,38 @@ function encrypt(plainText, key) {
             34, 2, 42, 10, 50, 18, 58, 26,
             33, 1, 41, 9, 49, 17, 57, 25];
     
-    let permuted = permute(plainText, initialPermutation);
-    
-    let leftPlainText = '';
-    
-    let rightPlainText = '';
-    
-    
 
-    for(let i = 0; i < 64; i++){
-        
-        if(i < 32)
-            leftPlainText += permuted[i];
-        
-        else if (i > 32 && i < 64)
-            rightPlainText += permuted[i];
+    let permutedText = permute(plainText, initialPermutation);
+    
+    let left = permutedText.slice(0, 32);
 
+    let right = permutedText.slice(32, 64);
+    
+    for (let round = 0; round < 16; round++) {
+        let newRight = (parseInt(left, 2) ^ parseInt(f(right, keys[round]), 2)).toString(2).padStart(32, '0');
+        left = right;
+        right = newRight;
     }
-    key = hexToBinary(key);
-
-    dropedKey = parityDrop(key);
-
-    let roundKeys = generateRoundKey(dropedKey);
-
-    for(let round = 0; round < 16; round++){
-        [left, right] = roundEncrypt(leftPlainText, rightPlainText, roundKeys[round]);
-    }
-
-    combinedText = rightPlainText + leftPlainText;
-
-    let cipherText = parseInt(combinedText, 2).toString(16).toUpperCase().padStart(16, '0');
-
-    return cipherText;
+    
+    let combinedText = right + left;
+    
+    return permute(combinedText, finalPermutationTable);
 }
+function encrypt(plainText, key) {
+    
+    let binaryKey = hexToBinary(key);
+
+    let dropedKey = parityDrop(binaryKey);
+
+    let roundKeys = generateRoundKeys(dropedKey);
+    
+    let binaryPlainText = hexToBinary(plainText);
+
+    let encryptedText = encryptBlock(binaryPlainText, roundKeys);
+
+    return parseInt(encryptedText, 2).toString(16).toUpperCase().padStart(16, '0');
+}
+const plainText = "0123456789ABCDEF";
+const key = "133457799BBCDFF1";
+const encryptedText = encrypt(plainText, key);
+console.log("Encrypted Text:", encryptedText);
